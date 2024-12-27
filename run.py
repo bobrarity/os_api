821fe990d3bee1f2da87d1509e72a4cb649811be
bobc7sec <bob@c7-security.com>
2024-12-27 09:45:22 +0500

    11

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException, Request, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from database import *
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 connected_clients = []
 
